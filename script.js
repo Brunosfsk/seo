07c59046b9d59fd27dfa709e08f09d92cc9d1e7c
Bruno Ferreira <brunosfsk8@hotmail.com>
2023-10-12 07:05:28 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// alert("conectado");
+
+
+        // const teste1 = document.querySelector(".box_cadastro");
+        // var teste2 = document.querySelector(".box_cadastro");
+        // let teste3 = document.querySelector(".box_cadastro");
+
+        var volume = document.querySelector(".volume");
+        // var inputvolume = volume.value;  
+        var sd = document.querySelector(".sd");
+        var ca = document.querySelector(".ca");
+        var data = document.querySelector(".data");
+        var cadastrar = document.querySelector(".cadastrar");
+        
+        var p = document.querySelector(".pResult")
+        var p1 = document.querySelector(".pResult1")
+        var p2 = document.querySelector(".pResult2")
+        var p3 = document.querySelector(".pResult3")
+        var p4 = document.querySelector(".pResult4")
+        // var valor = dados.value;
+
+        
+
+        function chamar() {
+            
+            p.innerHTML = volume.value;
+            p1.innerHTML = sd.value;
+            p2.innerHTML = ca.value;
+            p3.innerHTML = data.value;
+            p4.innerHTML = volume.value;
+            console.log("ok")
+        };
+        
